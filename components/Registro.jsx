864d915c5d23cd9dd1e99f66e3c00e00395dec48
JasnDev <jasaniastadeu@gmail.com>
2024-11-01 14:03:32 -0300

    Update registro, validação email, e senha.

--- components/Registro.jsx
+++ components/Registro.jsx
@@ -1,38 +1,77 @@
-import React from "react";
-import { StyleSheet, TextInput, View, Pressable, Text } from "react-native";
+import React, {useState} from "react";
+import { StyleSheet, TextInput, View, Pressable, Text, Alert } from "react-native";
 import { Link } from "expo-router";
 
 const Registro = () => {
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    const validacaoDados = () => {
+        if (!emailRegex.test(email)) {
+            setError('Email inválido.');
+            return false;
+        }
+        if (senha.length < 6) {
+            setError('A senha deve ter pelo menos 6 caractéres.');   
+            return false;
+        }
+        else {
+            setError('')
+            return true;
+        };
+    };
+
     return (
-        <View style={styles.container}>
+        <View style={styles.container} accessible={true} accessibilityLabel="Tela de registro do usuário.">
             <Text style={styles.title}>Registro</Text>
 
-            <Text style={styles.label}>Nome de Usuário: </Text>
+            <Text style={styles.label} accessibilityLabel="Nome do usuário.">Nome de Usuário: </Text>
             <TextInput
                 style={styles.input}
                 placeholder="Insira o nome de usuário."
+                accessibilityLabel="Campo de texto para inserir o nome do usuário."
             />
 
             <Text style={styles.label}>E-mail: </Text>
             <TextInput
                 style={styles.input}
                 placeholder="Insira seu e-mail."
+                value={email}
+                onChangeText={(text) =>{
+                    setEmail(text);
+                    validacaoDados();
+                }}
+                accessibilityLabel="Campo de texto para inserir seu e-mail."
+                keyboardType="email-address"
             />
 
-            <Text style={styles.label}>Senha: </Text>   
+            <Text style={styles.label} accessibilityLabel="Campo de texto para inserir sua senha.">Senha: </Text>   
             <TextInput 
                 style={styles.input}
                 placeholder="Insira sua senha."
+                accessibilityLabel="Campo de texto para inserir sua senha."
+                onChangeText={(text) =>{
+                    setSenha(text);
+                    validacaoDados();
+                }}
             />
 
             <Text style={styles.label}>Confirme sua senha: </Text>
             <TextInput 
                 style={styles.input}
                 placeholder="Confirme sua senha."
+                accessibilityLabel="Confirme sua senha."
             />
 
+            {error ? 
+            <Text style={styles.errorText} 
+            accessibilityLabel={error}>{error}</Text> 
+            : null}
+
             <Link href={'/login'}>
-                <Pressable style={styles.button}>
+                <Pressable style={styles.button} accessibilityLabel="botão para confirmar o registro">
                     <Text style={styles.buttonText}>Confirme</Text>
                 </Pressable>
             </Link>
@@ -84,12 +123,17 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.3,
         shadowRadius: 3,
         marginBottom: 10,
+        marginTop: 5,
     },
     buttonText: {
         fontSize: 18,
         color: '#fff',
         fontWeight: 'bold',
     },
+    errorText: {
+        fontSize: 15,
+        color: 'red',
+    }
 });
 
 export default Registro;
