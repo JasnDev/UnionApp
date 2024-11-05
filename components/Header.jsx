9cd87a729af66717cc84042d3d01073357ce4a6a
KEVIN00017 <162355670+KEVIN00017@users.noreply.github.com>
2024-11-05 17:07:28 -0300

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,10 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-<<<<<<< HEAD
+
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { View, StyleSheet, Pressable,Text } from 'react-native';
-=======
+
 import { View, StyleSheet, Pressable, Text } from 'react-native';
->>>>>>> 53076ecc69a3cf41d289552788b357d668ea9926
+
 import { useNavigation } from '@react-navigation/native';
 
 const Header = () => {
@@ -16,16 +16,16 @@ const Header = () => {
                     <FontAwesome name="user-o" size={24} color="#000" />
                 </Pressable>
 
-<<<<<<< HEAD
+
                 <Pressable style={styles.iconCenter} onPress={() => navigation.navigate('post')}>
                     <AntDesign name="pluscircleo" size={35} color="#000" />
                 </Pressable>
                 <Pressable style={styles.iconCenter} onPress={() => localStorage.clear()}>
                       <Text> Sair </Text>
-=======
+
                 <Pressable style={styles.iconCenter} onPress={() => localStorage.clear()}>
                       <Text>Sair</Text>
->>>>>>> 53076ecc69a3cf41d289552788b357d668ea9926
+
                 </Pressable>
                 
         </View>
